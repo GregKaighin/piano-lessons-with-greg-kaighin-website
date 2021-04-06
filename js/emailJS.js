c3dde7456690bcdfc463b6e8a2032a9b7e1843b0
GregKaighin <gregkaighin@gmail.com>
2021-04-06 21:25:46 +0000

    Form test

--- js/emailJS.js
+++ js/emailJS.js
@@ -1,4 +1,4 @@
-emailjs.sendForm('service_s6y906n', 'template-piano-lessons', 'contact-form')
+emailjs.sendForm('service_s6y906n', 'template-piano-lessons', '#contact-form')
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
