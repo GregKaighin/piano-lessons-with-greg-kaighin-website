abe9ee8b585a959271a334e9bb6a84f0d2b97426
GregKaighin <gregkaighin@gmail.com>
2021-04-25 21:03:19 +0000

    Fix email modal and apply box-shadow across pages

--- assets/js/emailJS.js
+++ assets/js/emailJS.js
@@ -7,11 +7,13 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            console.log('Success', response);
+            console.log("SUCCESS", response);
+            $('#thank-you-modal').modal('show');  // Opens the modal on submission of all completed form fields
+            $('#form-reset')[0].reset(); // Resets form fields on submission
         },
         function(error) {
-            console.log('Failed', error);
+            console.log("FAILED", error);
         }
-    )
-    return false;
+    );
+        return false; // Stops the page from refreshing
 }
